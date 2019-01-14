1480f171e92dc861efb73dccb1f8cad1a9d09383
Ajay Balakumaran <ajayececit@gmail.com>
2019-01-14 13:21:27 +0530

    Add files via upload

---
+++ weatherBot.py
@@ -0,0 +1,183 @@
+
+import telebot
+from telegram import ReplyKeyboardMarkup
+from telebot import types
+import telegram
+from weatherClass import forecastByCity
+from weatherClass import currentWeatherByCity
+bot_token = "636067336:AAFXGEEn9IG_qU8BNqpJnQKpTVzOvyA_6rY"
+bot = telebot.TeleBot(bot_token)
+
+# Unicodes for Emoji for respective weather code
+thunderstorm = u'\U0001F4A8'    # Code: 200's, 900, 901, 902, 905
+drizzle = u'\U0001F4A7'         # Code: 300's
+rain = u'\U00002614'            # Code: 500's
+snowflake = u'\U00002744'       # Code: 600's snowflake
+snowman = u'\U000026C4'         # Code: 600's snowman, 903, 906
+atmosphere = u'\U0001F301'      # Code: 700's foogy
+clearSky = u'\U00002600'        # Code: 800 clear sky
+fewClouds = u'\U000026C5'       # Code: 801 sun behind clouds
+clouds = u'\U00002601'          # Code: 802-803-804 clouds general
+hot = u'\U0001F525'             # Code: 904
+defaultEmoji = u'\U0001F300'    # default emojis
+
+degree_sign= u'\N{DEGREE SIGN}' # degree sign
+
+footer = "------------------------------------------------------------------"
+
+forecastList = []
+print ("bfre class")
+
+def inputKeyboard():
+    print ("keyClass")
+    markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True, resize_keyboard=True)
+    markup.add("Current Weather", "ForeCast")
+    markup.add("<<Back")
+    return markup
+
+def weatherInputKeyboard():
+    print ("keyClass")
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup.add("Pin Location", "City")
+    return markup
+    
+@bot.message_handler(commands=['start', 'help'])
+def handle_command_testMessage(message):
+    if (message.text == "/start"):
+        bot.send_message(chat_id=message.chat.id,text = "**Please select from the below menu**" ,reply_markup=weatherInputKeyboard(), parse_mode=telegram.ParseMode.MARKDOWN)
+        @bot.message_handler(regexp='City')
+        def handle_command_weather(message):
+            if(message.text == "City"):
+                echo = bot.reply_to(message,text = "Please provide the city name \n eg:Bangalore,IN")
+                bot.register_next_step_handler(echo, processByCityName)
+        @bot.message_handler(regexp='Pin Location')
+        def handle_command_weather(message):
+            if(message.text == "Pin Location"):
+                echo = bot.reply_to(message, text = "Please Attach the Location using telegram Maps")
+                bot.register_next_step_handler(echo, handle_Location)
+            
+    if(message.text == "/help"):
+        bot.send_message(chat_id=message.chat.id,text = "test success")
+        
+@bot.message_handler(content_types=['location'])
+def handle_Location(message):
+    lat = str(message.location.latitude)
+    lon = str(message.location.longitude)
+    locToPass = lat + "@#$" + lon
+    bot.reply_to(message, text = "Please select the required" ,reply_markup=inputKeyboard(), parse_mode=telegram.ParseMode.MARKDOWN)
+    @bot.message_handler(regexp='Current Weather')
+    def handle_command_weather(message):
+        if(message.text == "Current Weather"):
+            weatherOutput = extractDetails(locToPass)
+            bot.reply_to(message, text = weatherOutput)
+            bot.send_message(chat_id=message.chat.id,text = footer ,reply_markup=weatherInputKeyboard(), parse_mode=telegram.ParseMode.MARKDOWN)
+        
+    @bot.message_handler(regexp='ForeCast')
+    def handle_command_weather(message):
+        if(message.text == "ForeCast"):
+            forecastOutput = []
+            forecastOutput = extractForecastDetails(locToPass)
+            forecastSplit = forecastOutput[0:3]
+            forecastToSend = forecastSplit[0] + forecastSplit [1] + forecastSplit[2]
+            bot.reply_to(message, text = forecastToSend)
+            bot.send_message(chat_id=message.chat.id,text = footer ,reply_markup=weatherInputKeyboard(), parse_mode=telegram.ParseMode.MARKDOWN)
+        
+    @bot.message_handler(regexp='<<Back')
+    def handle_command_weather(message):
+        if(message.text == "<<Back"):
+            bot.send_message(chat_id=message.chat.id,text = "**Please select from the below menu**" ,reply_markup=weatherInputKeyboard(), parse_mode=telegram.ParseMode.MARKDOWN)
+        
+    
+def processByCityName(message):
+    cityName = message.text
+    bot.reply_to(message, text = "Please select the required" ,reply_markup=inputKeyboard(), parse_mode=telegram.ParseMode.MARKDOWN)
+    @bot.message_handler(regexp='Current Weather')
+    def handle_command_weather(message):
+        if(message.text == "Current Weather"):
+            weatherOutput = extractDetails(cityName)
+            bot.reply_to(message, text = weatherOutput)
+            bot.send_message(chat_id=message.chat.id,text = footer ,reply_markup=weatherInputKeyboard(), parse_mode=telegram.ParseMode.MARKDOWN)
+        
+    @bot.message_handler(regexp='ForeCast')
+    def handle_command_weather(message):
+        if(message.text == "ForeCast"):
+            forecastOutput = []
+            forecastOutput = extractForecastDetails(cityName)
+            forecastSplit = forecastOutput[0:3]
+            forecastToSend = forecastSplit[0] + forecastSplit [1] + forecastSplit[2]
+            bot.reply_to(message, text = forecastToSend)
+            bot.send_message(chat_id=message.chat.id,text = footer ,reply_markup=weatherInputKeyboard(), parse_mode=telegram.ParseMode.MARKDOWN)
+        
+    @bot.message_handler(regexp='<<Back')
+    def handle_command_weather(message):
+        if(message.text == "<<Back"):
+            bot.send_message(chat_id=message.chat.id,text = "**Please select from the below menu**" ,reply_markup=weatherInputKeyboard(), parse_mode=telegram.ParseMode.MARKDOWN)
+    
+
+def extractDetails(cityDetails):
+    try:
+        weatherClass = currentWeatherByCity(cityDetails)
+        weatherCode = weatherClass.weatherCode
+        weatherDesc = weatherClass.weatherDescription
+        weatherEmoji = getEmoji(weatherCode)
+        currentTemperature = str(weatherClass.currentTemp)
+        maxTemperature = str(weatherClass.maxTemp)
+        minTemperature = str(weatherClass.minTemp)
+        humidity = str(weatherClass.humidity)
+        windSpeed = str(weatherClass.windSpeed)
+        weatherOutput = "Weather : " + weatherDesc + " " + weatherEmoji  + "\n-> Current Temperature : " + currentTemperature + degree_sign + "C" + "\n-> Max/Min Temperature : " + maxTemperature + degree_sign + "C" + "/" + minTemperature + degree_sign + "C" + "\n-> Humidity : " + humidity + "\n-> Wind Speed : " + windSpeed
+    except:
+        weatherOutput = "City Not Found"
+    return weatherOutput
+
+def extractForecastDetails(cityDetails):
+    count = 0
+    while(count < 23):
+        weatherCode = ""
+        weatherDesc = ""
+        weatherEmoji = ""
+        maxTemperature = ""
+        minTemperature = ""
+        humidity = ""
+        windSpeed = ""
+        forecastCollection = ""
+        
+        forecastClass = forecastByCity(cityDetails ,count)
+        weatherDesc = forecastClass.forecastDescription
+        weatherEmoji = getEmoji(weatherCode)
+        maxTemperature = str(forecastClass.maxTemp)
+        minTemperature = str(forecastClass.minTemp)
+        humidity = str(forecastClass.humidity)
+        windSpeed = str(forecastClass.windSpeed)
+        date = str(forecastClass.date)
+    	
+        forecastCollection = "\nThe weather Report on Date : " + date + "\nWeather : " + weatherDesc + " " + weatherEmoji + "\n->Max/Min Temp : " + maxTemperature + degree_sign + "C" + "/" + minTemperature + degree_sign + "C" + "\n-> Humidity : " + humidity + "\n-> Wind Speed : " + windSpeed + "\n"
+        forecastList.append(forecastCollection)
+        count = count + 8   
+
+    return forecastList[0:3]
+       
+def getEmoji(weatherCode):
+    if (weatherCode):
+        if str(weatherCode)[0] == '2' or weatherCode == 900 or weatherCode==901 or weatherCode==902 or weatherCode==905:
+            return thunderstorm
+        elif str(weatherCode)[0] == '3':
+            return drizzle
+        elif str(weatherCode)[0] == '5':
+            return rain
+        elif str(weatherCode)[0] == '6' or weatherCode==903 or weatherCode== 906:
+            return snowflake + ' ' + snowman
+        elif str(weatherCode)[0] == '7':
+            return atmosphere
+        elif weatherCode == 800:
+            return clearSky
+        elif weatherCode == 801:
+            return fewClouds
+        elif weatherCode==802 or weatherCode==803 or weatherCode==803:
+            return clouds
+        elif weatherCode == 904:
+            return hot    
+    else:
+        return defaultEmoji
+    
+bot.polling()
---
+++ weatherClass.py
@@ -0,0 +1,137 @@
+
+import requests
+import pandas as pd
+import json
+
+class forecastByCity(object):
+    
+    def __init__(self, location ,count):
+        openWeatherAPI_key = "f0983710160711b7b3010621ea077e80"
+        openForecastUrl= "http://api.openweathermap.org/data/2.5/forecast?"
+        if (location.find("@#$") == -1):
+            openForecastByCity = openForecastUrl + "q=" + location + "&APPID=" + openWeatherAPI_key
+        else:
+            locationSplit = location.split("@#$")
+            latlonConcatenate = "lat=" + locationSplit[0] + "&lon=" + locationSplit[1]                               
+            openForecastByCity = openForecastUrl + latlonConcatenate + "&APPID=" + openWeatherAPI_key
+            
+        forecastData = requests.get(openForecastByCity)
+        if (forecastData.status_code ==  200):            
+            self.forecastJson = forecastData.json() 
+            self.forecastCode = self.__forecastCode(count)
+            self.forecastDescription = self.__forecastDescription(count)
+            self.currentTemp = self.__currentTemp(count)
+            self.maxTemp = self.__maxTemp(count)
+            self.minTemp = self.__minTemp(count)
+            self.humidity= self.__humidity(count)
+            self.windSpeed = self.__windSpeed(count)
+            self.date = self.__date(count)
+
+        elif(forecastData.status_code == 401):
+            return "Issue with API key"
+        elif(forecastData.status_code == 404):
+            return "Page not found"        
+    
+    def __forecastCode(self,count):
+        forecastTable = self.forecastJson["list"]
+        forecastDataFrame = pd.DataFrame(forecastTable)
+        idTable = forecastDataFrame.iloc[count]["weather"]
+        IdFrame = pd.DataFrame(idTable)
+        return IdFrame.iloc[0]["id"]
+    
+    def __forecastDescription(self,count):
+        forecastTable = self.forecastJson["list"]
+        forecastDataFrame = pd.DataFrame(forecastTable)
+        idTable = forecastDataFrame.iloc[count]["weather"]
+        IdFrame = pd.DataFrame(idTable)
+        return IdFrame.iloc[0]["description"]
+    
+    def __currentTemp(self,count):
+        forecastTable = self.forecastJson["list"]
+        forecastDataFrame = pd.DataFrame(forecastTable)
+        currentTemp = (forecastDataFrame.iloc[count]["main"])
+        return (round(currentTemp["temp"] -273.15 , 2))
+    
+    def __windSpeed(self,count):
+        forecastTable = self.forecastJson["list"]
+        forecastDataFrame = pd.DataFrame(forecastTable)
+        windSpeed =  (forecastDataFrame.iloc[count]["wind"])    
+        return windSpeed["speed"]
+    
+    def __humidity(self,count):
+        forecastTable = self.forecastJson["list"]
+        forecastDataFrame = pd.DataFrame(forecastTable)
+        humidity = (forecastDataFrame.iloc[count]["main"])
+        return humidity["humidity"]
+    
+    def __maxTemp(self,count):
+        forecastTable = self.forecastJson["list"]
+        forecastDataFrame = pd.DataFrame(forecastTable)
+        maxTemp = (forecastDataFrame.iloc[count]["main"])
+        return (round(maxTemp["temp_max"]-273.15 , 2))
+            
+    def __minTemp(self,count):
+        forecastTable = self.forecastJson["list"]
+        forecastDataFrame = pd.DataFrame(forecastTable)
+        minTemp =  (forecastDataFrame.iloc[count]["main"])
+        return (round(minTemp["temp_min"]-273.15 , 2))
+      
+    def __date(self,count):
+        forecastTable = self.forecastJson["list"]
+        forecastDataFrame = pd.DataFrame(forecastTable)
+        date = (forecastDataFrame.iloc[count]["dt_txt"])
+        date = date.split(" ")
+        return date[0]
+    
+class currentWeatherByCity(object):
+    def __init__(self, location):        
+        openWeatherAPI_key = "f0983710160711b7b3010621ea077e80"
+        openWeatherUrl= "http://api.openweathermap.org/data/2.5/weather?"
+        if (location.find("@#$") == -1):
+            openWeatherByCity = openWeatherUrl + "q=" + location + "&APPID=" + openWeatherAPI_key
+        else:
+            locationSplit = location.split("@#$")
+            latlonConcatenate = "lat=" + locationSplit[0] + "&lon=" + locationSplit[1]
+            openWeatherByCity = openWeatherUrl + latlonConcatenate + "&APPID=" + openWeatherAPI_key
+        
+        weatherData = requests.get(openWeatherByCity)
+        if (weatherData.status_code ==  200):            
+            self.weatherJson = weatherData.json()  
+            self.weatherCode = self.__weatherCode()
+            self.weatherDescription = self.__weatherDescription()
+            self.currentTemp = self.__currentTemp()
+            self.maxTemp = self.__maxTemp()
+            self.minTemp = self.__minTemp()
+            self.humidity= self.__humidity()
+            self.windSpeed = self.__windSpeed()
+        elif(weatherData.status_code == 401):
+            return "Issue with API key"
+        elif(weatherData.status_code == 404):
+            return "Page not found"
+        
+    def __weatherCode(self):
+        weatherTable = self.weatherJson["weather"]
+        weatherDataFrame = pd.DataFrame(weatherTable)
+        return (weatherDataFrame.iloc[0]['id'])
+    
+    def __weatherDescription(self):
+        weatherTable = self.weatherJson["weather"]
+        weatherDataFrame = pd.DataFrame(weatherTable)
+        return (weatherDataFrame.iloc[0]['description'])
+        
+    def __currentTemp(self):
+        return (round(self.weatherJson['main']['temp']-273.15 , 2))
+    
+    def __maxTemp(self):
+        return (round(self.weatherJson['main']['temp_max']-273.15 , 2))
+    
+    def __minTemp(self):
+        return (round(self.weatherJson['main']['temp_min']-273.15 , 2))
+    
+    def __humidity(self):
+        return (self.weatherJson['main']['humidity'])
+    
+    def __windSpeed(self):
+        return (self.weatherJson['wind']['speed'])
+    
+    
